c419919c8ca0e7cb31b39535aab8c59f5c52ce54
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-12-01 12:11:21 -0500

    Developing providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
