258a89dfba1a05737b738080c2b53b5d237abb46
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-12-01 10:55:24 -0500

    Adding more buttons and icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,12 +2,12 @@
 import React from 'react';
 
 // Icons
-import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon } from '@heroicons/react/outline';
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon } from '@heroicons/react/outline';
 
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='text-gray-500 p-5 text-sm border-r-gray-900'>
       {/* option + click = multicursor */}
       <button className='flex items-center space-x-2 hover:text-white'>
         {/* Height and width are required in Tailwind to show buttons */}
@@ -22,7 +22,29 @@ const Sidebar = () => {
         <LibraryIcon className='h-5 w-5' />
         <p>Your Library</p>
       </button>
+
+      {/* Border on the top w/ custom value of 0.1 pixels */}
+      {/* 'Custom value' referring to value within [] brackets - just in time compiler mode */}
+      {/* Compiler mode takes custom values and creates utility classes on the fly to support dev */}
+      <hr className='border-t-[0.1px] border-gray-900'/>
+
+      <button className='flex items-center space-x-2 hover:text-white'>
+        <PlusCircleIcon className='h-5 w-5' />
+        <p>Create Playlist</p>
+      </button>
+      <button className='flex items-center space-x-2 hover:text-white'>
+        <HeartIcon className='h-5 w-5' />
+        <p>Liked Songs</p>
+      </button>
+      <button className='flex items-center space-x-2 hover:text-white'>
+        <RssIcon className='h-5 w-5' />
+        <p>Your Episodes</p>
+      </button>
+      
+      <hr className='border-t-[0.1px] border-gray-900'/>
     </div>
+
+    
   );
 };
 
