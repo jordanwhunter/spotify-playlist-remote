bd751e31f068c2ec1dac7a9049928d550f6bf59c
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-12-01 10:40:22 -0500

    Adding buttons to Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,26 @@
+// Dependencies
 import React from 'react';
+
+// Icons
+import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon } from '@heroicons/react/outline';
+
 
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+      <button>
+        {/* Height and width are required in Tailwind to show buttons */}
+        <HomeIcon className='h-5 w-5' />
+        <p>Home</p>
+      </button>
+      <button>
+        <SearchIcon className='h-5 w-5' />
+        <p>Search</p>
+      </button>
+      <button>
+        <LibraryIcon className='h-5 w-5' />
+        <p>Your Library</p>
+      </button>
     </div>
   );
 };
